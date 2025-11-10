ada3b21e44d944247089439c4f50a5e4d8580e48
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-11-10 20:20:20 -0300

    Update App.jsx

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import { useDarkMode } from './hooks/useDarkMode'
+
+function App() {
+  const [isDark] = useDarkMode()
+
+  return (
+    <Router>
+      <div className={`min-h-screen flex flex-col ${isDark ? 'dark' : ''}`}>
+        <div className="flex-grow bg-warm-white dark:bg-neutral-bg transition-colors">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              {/* Adicionaremos mais rotas depois */}
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </div>
+    </Router>
+  )
+}
+
+export default App
