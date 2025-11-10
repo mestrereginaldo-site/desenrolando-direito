da936fa2dcb68e19a831048f2b916a6af0b7d461
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-11-10 20:33:57 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,6 @@ function App() {
           <main>
             <Routes>
               <Route path="/" element={<Home />} />
-              {/* Adicionaremos mais rotas depois */}
             </Routes>
           </main>
           <Footer />
