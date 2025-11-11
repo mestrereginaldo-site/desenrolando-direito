edfc8f7c2cd2f159e39c2b8366011377fb72bd81
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-11-10 21:26:12 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  },
-  server: {
-    port: 3000
+    emptyOutDir: true
   }
 })
